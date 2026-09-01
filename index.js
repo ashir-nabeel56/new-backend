c7faa279e90f6913b8c4a619dcb46652787803cc
Nabeel <nabeelajmer@gmail.com>
2026-09-01 18:04:43 +0500

    something updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
@@ -22,6 +23,12 @@ app.use("/products", productRoutes);
 // Home
 app.get("/", (req, res) => {
   res.send("E-Commerce Backend is running");
+});
+
+// Server start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // IMPORTANT: Vercel ke liye app export karo
